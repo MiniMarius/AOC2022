9a644c8f8c2494552d2e0264a342e32396285325
MiniMarius <seffermarius@gmail.com>
2022-12-01 11:40:13 +0100

    second part finished

--- day1/CalorieCounter.py
+++ day1/CalorieCounter.py
@@ -9,6 +9,12 @@ with open("input.txt", "r") as file:
             elfCalories.append(calorieCount)
             calorieCount = 0
 
+
 max_calories = max(elfCalories)
 max_index = elfCalories.index(max_calories)
+#sorting for part two
+sortedCals = sorted(elfCalories, reverse=True)
+topThree = sum(sortedCals[:3])
+
 print("Most calories carried by the " + str(max_index) + "th elf, " + "calories carried: " + str(max_calories))
+print("Top three: " + str(topThree))
